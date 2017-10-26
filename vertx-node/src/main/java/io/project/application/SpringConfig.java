abc2dab0e77cd418dbaf53a97ffb40d59177d897
armdev <armen.arzumanyan@gmail.com>
2017-10-26 22:51:17 +0400

    enable swagger

--- vertx-node/src/main/java/io/project/application/SpringConfig.java
+++ vertx-node/src/main/java/io/project/application/SpringConfig.java
@@ -1,11 +1,13 @@
 package io.project.application;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
+
 @Import(SwaggerConfig.class)
 public class SpringConfig extends WebMvcConfigurerAdapter {
 

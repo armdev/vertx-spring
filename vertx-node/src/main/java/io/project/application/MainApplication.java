c3a8c0bdd4d348277216a74f4e01f44187be85e9
armdev <armen.arzumanyan@gmail.com>
2017-10-26 22:47:51 +0400

    enable swagger

--- vertx-node/src/main/java/io/project/application/MainApplication.java
+++ vertx-node/src/main/java/io/project/application/MainApplication.java
@@ -9,13 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @Configuration
 @EnableJpaRepositories("io.project.repositories")
-@ComponentScan(basePackages = { "io.project" })
+@ComponentScan(basePackages = {"io.project"})
 @EntityScan("io.project.model")
+@Import(SpringConfig.class)
 public class MainApplication {
 
     public static void main(String[] args) {
@@ -27,7 +29,7 @@ public class MainApplication {
 
     @PostConstruct
     public void deployVerticle() {
-         Vertx.vertx().deployVerticle(staticServer);
+        Vertx.vertx().deployVerticle(staticServer);
 
     }
 
---
+++ vertx-node/src/main/java/io/project/application/SpringConfig.java
@@ -0,0 +1,20 @@
+package io.project.application;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Import(SwaggerConfig.class)
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
